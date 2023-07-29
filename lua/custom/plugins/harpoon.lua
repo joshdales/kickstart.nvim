b6974c63bb7e1ac29fe83c8273f4f138dce6c9c4
Josh Dales <git@joshdales.dev>
2023-07-29 07:14:15 -0400

    Add description to the Harpoon file:W

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,8 +1,8 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
 
-vim.keymap.set("n", "<leader>ha", mark.add_file)
-vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "[H]arpoon [A]dd" })
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle Harpoon quick menu" })
 
 return {
 	"ThePrimeagen/harpoon",
